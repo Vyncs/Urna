f9e762af18dbb9b0bdea57c40d4a8681296009eb
Vinicius Gonçalves <70669306+Vyncs@users.noreply.github.com>
2021-08-23 12:46:16 -0300

    Add files via upload

---
+++ Urna-main/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                name: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    {url: '2.jpeg', legenda: 'Vereador',}
+                ]
+            },
+            {
+                numero: '77222',
+                name: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos: [
+                    {url:'1.jpeg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                name: 'Ciclano',
+                partido: 'ABC',
+                vice:'Cic',
+                fotos: [
+                    {url: '3.png', legenda: 'Prefeito'},
+                    {url: '4.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                name: 'Zulano',
+                partido: 'QWERTY',
+                vice:'Zul',
+                fotos: [
+                    {url: '5.png', legenda: 'Prefeito'},
+                    {url: '6.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+]
